acbd1e2b56f0c6cc2b6a60a189c56d1767916a79
RUBESH <rubeshkriz@gmail.com>
2020-08-21 13:43:58 +0530

    basic-setup

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/login">
+            <h1>LOGIN PAGE</h1>
+          </Route>
+          <Route path="/checkout">
+            <h1>CHECKOUT PAGE</h1>
+          </Route>
+          <Route path="/">
+            <h1>HOME PAGE</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
